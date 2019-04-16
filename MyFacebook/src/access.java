abfc8abdfe821d4e0e968e004ed222005d9c1e10
lemowery <levimowery@outlook.com>
2019-04-16 15:14:21 -0400

    Removed unused imports

--- MyFacebook/src/access.java
+++ MyFacebook/src/access.java
@@ -1,15 +1,12 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.jar.Attributes.Name;
 
 public class access {
 
